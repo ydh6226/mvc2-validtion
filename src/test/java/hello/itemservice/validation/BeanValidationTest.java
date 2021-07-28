5f236eff3dadc25efd6209cdd95af9b097e32b4a
ydh6226 <ydh6226@naver.com>
2021-07-29 08:42:37 +0900

    Bean validation 직접 사용

---
+++ src/test/java/hello/itemservice/validation/BeanValidationTest.java
@@ -0,0 +1,30 @@
+package hello.itemservice.validation;
+
+
+import hello.itemservice.domain.item.Item;
+import org.junit.jupiter.api.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import java.util.Set;
+
+public class BeanValidationTest {
+
+    @Test
+    void beanValidationTest() {
+        Validator validator = Validation.buildDefaultValidatorFactory()
+                .getValidator();
+
+        Item item = new Item();
+        item.setItemName(" ");
+        item.setPrice(0);
+        item.setQuantity(10000);
+
+        Set<ConstraintViolation<Item>> violations = validator.validate(item);
+        for (ConstraintViolation<Item> violation : violations) {
+            System.out.println("violation = " + violation);
+            System.out.println("violation.getMessage() = " + violation.getMessage());
+        }
+    }
+}
